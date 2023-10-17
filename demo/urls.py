bfa7e8ae04edca1c112d09ba961a548ba2cacd6f
Rafael Silva <nightmarezlolzin@gmail.com>
2023-10-17 09:53:14 -0300

    Update requirements.txt and collectstatic command

--- demo/urls.py
+++ demo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from demo.demo import settings
+from demo import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
