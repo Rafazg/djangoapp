db5a9b81a414ccdca6e9dab54d86b745356dbf60
Rafael Silva <nightmarezlolzin@gmail.com>
2023-11-10 14:47:46 -0300

    Update static files settings

--- demo/settings.py
+++ demo/settings.py
@@ -146,5 +146,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '.vercel_build_output', 'static')
